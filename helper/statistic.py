27288847cad1212edacfee39350a82c4434ebd83
mickaelyoshua <mickaelyoshua@gmail.com>
2023-07-06 16:31:17 -0300

    start chapter 14

--- helper/statistic.py
+++ helper/statistic.py
@@ -39,6 +39,19 @@ def standard_deviation(xs: List[float]) -> float:
     return math.sqrt(variance(xs))
 assert 1.58 < standard_deviation([1,2,3,4,5]) < 1.59
 
+from helper.linear_algebra import dot
+def covariance(xs: List[float], ys: List[float]) -> float:
+    assert len(xs) == len(ys), "xs and ys must have same number of elements"
+    return dot(de_mean(xs), de_mean(ys)) / (len(xs) - 1)
+def correlation(xs: List[float], ys: List[float]) -> float:
+    """Measures how much xs and ys vary in tandem about their means"""
+    stdev_x = standard_deviation(xs)
+    stdev_y = standard_deviation(ys)
+    if stdev_x > 0 and stdev_y > 0:
+        return covariance(xs, ys) / stdev_x / stdev_y
+    else:
+        return 0    # if no variation, correlation is zero
+
 def initial_statistics(xs: List[float]) -> dict:
     """Return a dict with the 'Mean', 'Median' and 'Standard Deviation'"""
     assert len(xs) > 1, "The list must have more then one item"
