70a0d7af670815521dd1a522b6346e830774879f
mickaelyoshua <mickaelyoshua@gmail.com>
2023-07-07 15:38:33 -0300

    finished chapter 15

--- helper/linear_algebra.py
+++ helper/linear_algebra.py
@@ -2,6 +2,12 @@ import math
 from typing import List
 Vector = List[float]
 
+def add(v: Vector, w: Vector) -> Vector:
+    """Sum corresponding elements"""
+    assert len(v)==len(w), "Vectrs must be the same length"
+    return [v_i + w_i for v_i, w_i in zip(v, w)]
+assert add([1,2,3], [4,5,6]) == [5,7,9]
+
 def subtract(v: Vector, w: Vector) -> Vector:
     """Subtract corresponding elements"""
     assert len(v)==len(w), "Vectrs must be the same length"
